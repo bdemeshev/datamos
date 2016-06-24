7d0da617df2fbd13087f43bb610e64aa1b2db075
Boris Demeshev <boris.demeshev@gmail.com>
2016-06-24 19:53:44 +0300

    explicit package names, code styling, bug fixing

--- R/datamos.R
+++ R/datamos.R
@@ -2,59 +2,58 @@
 #'
 #' @name datamos
 #' @docType package
-#' @author Boris Demeshev 
-#' @import jsonlite RCurl
+#' @author Boris Demeshev
 NULL
 
 #' Download the data.frame of all datasets available at data.mos.ru
-#' 
+#'
 #' Download the data.frame of all datasets available at data.mos.ru
 #'
 #' Download the data.frame of all datasets available at data.mos.ru
-#' 
+#'
 #' @return the data.frame of all datasets available at data.mos.ru
 #' @export
 #' @examples
 #' dsets <- datamos_list()
 datamos_list <- function() {
   url <- "http://api.data.mos.ru/v1/datasets"
-  data <- getURL(url, .encoding = "UTF-8")
-  df <- fromJSON(data)
+  data <- RCurl::getURL(url, .encoding = "UTF-8")
+  df <- jsonlite::fromJSON(data)
   return(df)
 }
- 
+
 
 #' Download specific dataset from data.mos.ru
-#' 
+#'
 #' Download specific dataset from data.mos.ru
 #'
-#' Download specific dataset from data.mos.ru 
-#' 
+#' Download specific dataset from data.mos.ru
+#'
 #' @param id the integer id of dataset to download
 #' @return the desired dataset from data.mos.ru
 #' @export
 #' @examples
 #' df <- datamos(673)
-#' attr(df,"info_df")
-#' attr(df,"info_vars")
+#' attr(df, "info_df")
+#' attr(df, "info_vars")
 datamos <- function(id) {
-  url <- paste0("http://api.data.mos.ru/v1/datasets/",id,"/rows")
-  data_json <- getURL(url, .encoding = "UTF-8")
-  df <- fromJSON(data_json)
+  url <- paste0("http://api.data.mos.ru/v1/datasets/", id, "/rows")
+  data_json <- RCurl::getURL(url, .encoding = "UTF-8")
+  df <- jsonlite::fromJSON(data_json)
   cells <- df$Cells
   df$Cells <- NULL
-  df <- cbind(df,cells)
-    
-  url_info <- paste0("http://api.data.mos.ru/v1/datasets/",id)
-  info_json <- getURL(url_info, .encoding = "UTF-8")
-  info <- fromJSON(info_json)
-  
-  info_df <- as.data.frame(head(info,-1)) # info about data frame
-  info_vars <- tail(info,1)[[1]] # info about particular variables
-  
-  attr(df,"info_df") <- info_df
-  attr(df,"info_vars") <- info_vars
-  
+  df <- cbind(df, cells)
+
+  url_info <- paste0("http://api.data.mos.ru/v1/datasets/", id)
+  info_json <- RCurl::getURL(url_info, .encoding = "UTF-8")
+  info <- jsonlite::fromJSON(info_json)
+
+  info_df <- as.data.frame(t(unlist(head(info, -1)))) # info about data frame
+  info_vars <- tail(info, 1)[[1]] # info about particular variables
+
+  attr(df, "info_df") <- info_df
+  attr(df, "info_vars") <- info_vars
+
   return(df)
 }
 
